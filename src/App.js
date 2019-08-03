e7ee363404823940cdf160971f08d4a12e188864
Sitaram <sitaram@xelpmoc.in>
2019-08-03 17:04:54 +0530

    files_created

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import Newsfeed from './Newsfeed';
+function App()
+{
+  return (
+   <Newsfeed/>
+  )
+}
+export default App
---
+++ src/Newsfeed.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import styles from "./Newsfeed.module.css";
+import News from "./News";
+class Newsfeed extends Component {
+  state = {
+    todos: [],
+    error:false
+  };
+  componentDidMount() {
+    fetch(
+      "https://newsapi.org/v2/everything?q=bitcoin&from=2019-07-03&sortBy=publishedAt&apiKey=1f02327de8724a96aabfe0202a93a7c1"
+    )
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ todos: data.articles });
+      })
+      .catch(this.setState({error:true}))
+     
+  }
+  render() {
+    return (
+      <div className={styles.base}>
+      
+        {this.state.todos &&
+          this.state.todos.map(todo => (
+          <News image={todo.urlToImage} title={todo.title} content={todo.content}/> 
+          ))}
+          
+      </div>
+    );
+  }
+}
+export default Newsfeed;
+
+
